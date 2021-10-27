4ee09900410972c5974d0689a5e4b29868e1b8d5
Alessandro-Fedele <al.fedele@outlook.it>
2021-10-27 23:42:47 +0200

    esercizio fatto - con grafica non funziona ancora

---
+++ Richiesta esercizio/script/script.js
@@ -0,0 +1,72 @@
+//creo una lista di 5 numeri RANDOM E UNICI compresi fra 1 e 100
+const listaNumeri = [];
+let numeroRandom;
+
+while (listaNumeri.length < 5) {
+    numeroRandom = getRandom(100);
+    if (listaNumeri.indexOf(numeroRandom) === -1) {
+        listaNumeri.push(numeroRandom);
+    }
+}
+
+// metto i numeri in ordine
+listaNumeri.sort(compareNumbers);
+// alert con la lista dei numeri
+alert('Ricordati questi 5 numeri!: ' + listaNumeri);
+
+setTimeout(function ()
+{
+    let tentativo;
+    let listaNumeriRicordati = [];
+    let arrayTentativi = [];
+
+    // La condizione iniziale è che i tentativi sono 5
+    while (arrayTentativi.length < 5) {
+        tentativo = parseInt(prompt('Inserisci un numero da 1 a 100'));
+
+        //se l' utente inserisce due volte lo stesso numero gli si chiede di metterne un altro
+        if (arrayTentativi.includes(tentativo)) {
+            alert('Hai inserito due volte lo stesso numero! Riprova con un numero diverso!');
+
+            // se l' utente mette un numero NON compreso tra 1 e 100 gli si chiede di metterne un altro
+        } else if ((tentativo < 1) || (tentativo > 100)) {
+            alert('Numero non valido! Riprova con un altro tra 1 e 100');
+
+            // se utente usa caratteri non numerici gli chiedo di riprovare
+        } else if (isNaN(tentativo)) {
+            alert('Usa solo caratteri numerici!');
+
+        } else {
+            // se il tentativo dell'utente corrisponde a uno dei numeri iniziali, va nella lista dei numeri ricordati e nella lista dei tentativi totali
+            if (listaNumeri.includes(tentativo)) {
+                arrayTentativi.push(tentativo);
+                listaNumeriRicordati.push(tentativo);
+            } else {
+                // se il numero inserito non è uno di quei 5 che dovevo ricordare, va nell'arrayTentativi
+                arrayTentativi.push(tentativo);
+            }
+            //quanti numeri l'utente ha ricordato
+            var punti = listaNumeriRicordati.length;
+        }
+    }
+
+    alert('Ti sei ricordato ' + punti + ' numeri su 5');
+    alert('I numeri da ricordare erano: ' + listaNumeri);
+
+    if (listaNumeriRicordati.length > 0) {
+        alert('I numeri che ti sei ricodato sono: ' + listaNumeriRicordati);
+    } else
+        alert('Non ne hai ricordato nemmeno uno!!!');
+
+}, 3000); // metto 3.000ms invece di 30.000ms
+
+//numeri random
+function getRandom(max)
+{
+    return Math.floor(Math.random() * max) + 1;
+}
+//numeri in ordine
+function compareNumbers(a, b)
+{
+    return a - b;
+}
---
+++ Versione estetica/script/script.js
@@ -0,0 +1,94 @@
+"use strict";
+// Genero 5 numeri UNICI e RANDOM - da 1 a 100
+// Li metto in un array
+let randomNumbers = [];
+while (randomNumbers.length < 5) {
+    let num = Math.floor(Math.random() * 100) + 1;
+    if (randomNumbers.indexOf(num) === -1) randomNumbers.push(num);
+}
+
+// Button 
+const btnGenerateRandomNumbs = document.getElementById("btn-generate-numbers");
+// Container
+const numContainer = document.querySelector(".container");
+
+// L'utente genera i numeri al click
+btnGenerateRandomNumbs.addEventListener("click", function ()
+{
+    printRandomNumb();
+    // Parte il Timer
+    let counter = setInterval(timer, 1000);
+    let count = 2;
+    function timer()
+    {
+        count = count - 1;
+        if (count <= -1) {
+            clearInterval(counter);
+            stampForm();
+            return;
+        }
+        document.getElementById("timer").innerHTML = count;
+    }
+});
+
+function printRandomNumb()
+{
+    numContainer.innerHTML = "";
+    for (let i = 0; i < randomNumbers.length; i++) {
+
+        const number = randomNumbers[i];
+        numContainer.innerHTML += `<div>
+                                        <h3 class="px-5">${number}</h3>
+                                    </div>`;
+    }
+}
+
+// Numeri inseriti dall'utente - in un array
+let userNumbers = [];
+console.log(userNumbers.length);
+
+function stampForm()
+{
+    numContainer.innerHTML = `<form class="text-center">
+                                    <div class="mb-3 fs-4 fw-bold">
+                                        <label class="form-label">Quali numeri ricordi?</label>
+                                        <div class="input-group gap-5">
+                                            <input type="number" class="form-control inputs-numbers">
+                                            <input type="number" class="form-control inputs-numbers">
+                                            <input type="number" class="form-control inputs-numbers">
+                                            <input type="number" class="form-control inputs-numbers">
+                                            <input type="number" class="form-control inputs-numbers">
+                                        </div>
+                                        <div class="form-text text-danger">Inserisci un numero per ogni casella!</div>
+                                    </div>
+                                    <button type="button" id="input-btn" class="btn btn-primary">Submit</button>
+                                </form>`;
+    const btnSubmit = document.getElementById("input-btn");
+    btnSubmit.addEventListener("click", function ()
+    {
+        let inputNumber = document.getElementsByClassName("inputs-numbers");
+        for (var i = 0; i < inputNumber.length; ++i) {
+            if (typeof inputNumber[i].value !== "undefined") {
+                userNumbers.push(inputNumber[i].value);
+            }
+            console.log(userNumbers);
+        }
+
+
+        //Confronto l'array con i numeri inseriti dall'utente e l'array con i numeri random
+        let numeriRicordati = randomNumbers.filter(x => userNumbers.includes(x));
+        console.log(numeriRicordati);
+
+        const numeriNonRicordati = [];
+    });
+}
+
+// for (let i = 0; i < randomNumbers.length; i++) {
+//     if (randomNumbers.includes(userNumbers[i])) {
+//         numeriRicordati.push(userNumbers[i]);
+//         counter++;
+//     } else {
+//         numeriNonRicordati.push(userNumbers[i]);
+//     }
+//     numContainer.innerHTML = `I numeri che hai ricordato sono: ${numeriRicordati}, quelli che NON hai ricordato sono: ${numeriNonRicordati}`;
+// }
